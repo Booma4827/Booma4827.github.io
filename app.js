feb3adf3b257fed94574bef6dc26773cf6e6755b
Booma4827 <mypersonal4827@gmail.com>
2020-10-22 20:01:50 -0700

    Add JS file

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ y=a;
 console.log("x="+x,"y="+y);*/
 
 //Add a value at last item
-/*var a=[2,4,6,8,1];
+var a=[2,4,6,8,1];
 a.push(5,4);
 for(i=0;i<a.length;i++){
     console.log(a[i]);
-}*/
+}
 
 //Remove the last item
 /*var a=[2,4,6,8,4,1];
@@ -29,10 +29,10 @@ for(i=0;i<a.length;i++){
 }*/
 
 //Remove the first item
-var a=[1,3,5,7,9,2];
+/*var a=[1,3,5,7,9,2];
 a.shift();
 for(i=0;i<a.length;i++){
     console.log(a[i]);
-}
+}*/
 
 
