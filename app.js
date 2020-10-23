8ed2669ae5c0008eb45396b1057ac4b622089dbf
Booma4827 <mypersonal4827@gmail.com>
2020-10-23 07:33:41 -0700

    add a file

--- app.js
+++ app.js
@@ -56,23 +56,19 @@ console.log(a.lastIndexOf(2));
 var a=[1,3,5,7,9];
 console.log(a.indexOf(5));
 
-//splice
-var a=[2,5,7,89,1];
-for(i=0;i<a.length;i++)
-{
-    console.log(a.splice(2,0,4,8));
-}
-
 //join()
 var a=[2,4,6,8];
 var c=a.join();
 console.log(c);
 
-//sort()
-var a=[4,6,1,3,5];
-for(i=0;i<a.length;i++){
-    console.log(a.sort(a[i]));    
-}
+//includes
+var a=[2,4,6,8,1];
+console.log(a.includes(4,1));
+
+//indexof()
+var a=[2,3,4,6,7,4,2,3,4,7,3,4];
+console.log(a.indexOf(4));
+
 
 
 
