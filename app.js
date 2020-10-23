fc42fa9439bd32026f66691d823909ef3cb3125a
Booma4827 <mypersonal4827@gmail.com>
2020-10-22 20:37:02 -0700

    add new file

--- app.js
+++ app.js
@@ -8,31 +8,33 @@ y=a;
 console.log("x="+x,"y="+y);
 
 //Add a value at last item
-/*var a=[2,4,6,8,1];
+var a=[2,4,6,8,1];
 a.push(5,4);
 for(i=0;i<a.length;i++){
     console.log(a[i]);
-}*/
+}
 
 //Remove the last item
-/*var a=[2,4,6,8,4,1];
+var a=[2,4,6,8,4,1];
 a.pop();
 for(i=0;i<a.length;i++){
     console.log(a[i]);
-}*/
+}
 
 //Add a value at first item
-/*var a=[3,5,7,1,4];
+var a=[3,5,7,1,4];
 a.unshift(1);
 for(i=0;i<a.length;i++){
     console.log(a[i]);
-}*/
+}
 
 //Remove the first item
-/*var a=[1,3,5,7,9,2];
+var a=[1,3,5,7,9,2];
 a.shift();
 for(i=0;i<a.length;i++){
     console.log(a[i]);
-}*/
+}
+ 
+
 
 
